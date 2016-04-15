3b77328d2d3f96a085b82abe87b948937206e175
Vladimir Masko <soupdawgie@gmail.com>
2016-04-15 14:51:03 +0300

    Implemented primitive guest user, added DB seeds, fixed some styles

--- app/assets/images/Sign in _ Rubypaste_files/copy_link.self-60756a137700b6ea0874af080f4f2a1a757e634f2ec1d36eaeda0af523db1b08.js
+++
@@ -1,5 +0,0 @@
-// Launches the clipboard.js script
-new Clipboard('.copy');
-
-// Prevents from jumping to the top of the page after clicking a link
-$('a.copy').click(function(e) { e.preventDefault(); });
